ee85e114436eb9868274c08e4cc01ce57cf5a533
Harald Klimach <harald.klimach@dlr.de>
2025-05-20 01:09:52 +0200

    Pass '*' quoted to git config

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,10 @@
 
 echo "Building with Pyenv $ENV_VERSION"
 export PATH="$VIRTUAL_ENV/bin:$PATH"
+mkdir -p /github/home/.cache/pip
 env-freeze
 echo ""
-git config --global --add safe.directory *
+git config --global --add safe.directory '*'
 export OMPI_ALLOW_RUN_AS_ROOT=1
 export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 bin/waf --mpicmd="mpiexec --oversubscribe" $@
