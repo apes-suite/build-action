4b484e42f778915b72c33b25e900eb26858846e8
Harald Klimach <harald.klimach@dlr.de>
2025-05-20 15:14:46 +0200

    Introduce testing the action

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,16 +7,16 @@ echo ""
 echo ""
 echo "Copying to user workspace"
 mkdir -p /home
-cp -rfL . /home/apes
+cp -rfL $2 /home/apes
 chown -R apes /home/apes
 ORIGIN=$PWD
 cd /home/apes
-echo "Running bin/waf --mpicmd='mpiexec --oversubscribe' $@"
-runuser -u apes -- bin/waf --mpicmd="mpiexec --oversubscribe" $@
+echo "Running bin/waf --mpicmd='mpiexec --oversubscribe' $1"
+runuser -u apes -- bin/waf --mpicmd="mpiexec --oversubscribe" $1
 SUCCESS=$?
 cd $ORIGIN
 if [[ $SUCCESS ]]; then
   echo "Copying from user workspace"
-  cp -rfL /home/apes/build .
+  cp -rfL /home/apes/build $2
 fi
 exit $SUCCESS
