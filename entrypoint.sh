5aac415e568c648d1b7dbbd34f07bbaa23add0d0
Harald Klimach <harald.klimach@dlr.de>
2025-05-23 09:22:06 +0200

    Use chown rather than copying data around

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,12 @@ source $VIRTUAL_ENV/bin/activate
 env-freeze
 echo "::endgroup::"
 echo "::group::Build-Preparation"
-echo "Copying $2 to user workspace"
-mkdir -p /home
-cp -rfL $2 /home/apes
-chown -R apes /home/apes
+echo "Chowning to apes user"
+GHUSER=$(ls -ld . | awk 'NR==1 {print $3}')
+GHGROUP=$(ls -ld . | awk 'NR==1 {print $4}')
+chown -R apes .
 ORIGIN=$PWD
-cd /home/apes
+cd $2
 echo "::endgroup::"
 echo "::group::Building"
 echo "Running bin/waf --mpicmd='mpiexec --oversubscribe' $1"
@@ -20,17 +20,11 @@ SUCCESS=$?
 echo "::endgroup::"
 echo "::group::Build-Finalization"
 cd $ORIGIN
+echo "Chowning to GH user"
+chown -R $GHUSER:$GHGROUP $ORIGIN
 if [[ $SUCCESS ]]; then
   echo "* Successfully compiled \`$2\` with:" >> $GITHUB_STEP_SUMMARY
   echo "  \`bin/waf --mpicmd='mpiexec --oversubscribe' $1\`" >> $GITHUB_STEP_SUMMARY
-  echo "Copying $3 from user workspace to $ORIGIN/$2"
-  cp -rfL /home/apes/$3 $2
-  SUCCESS=$?
-  if [[ $SUCCESS ]]; then
-    echo "* Successfully copied \`$3\` back to workspace" >> $GITHUB_STEP_SUMMARY
-  else
-    echo "* But *FAILED* to copy resulting \`$3\`!" >> $GITHUB_STEP_SUMMARY
-  fi
 else
   echo "**FAILED** to compile \`$2\` with:" >> $GITHUB_STEP_SUMMARY
   echo "\`bin/waf --mpicmd='mpiexec --oversubscribe' $1\`" >> $GITHUB_STEP_SUMMARY
